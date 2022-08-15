ab1f3a45ff60c41a0446cf776239253b2a87d78b
sonu kumar sah <ssah6477@gmail.com>
2022-08-15 19:48:23 +0530

    first commit

---
+++ area of the circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    float a;
+    printf("Enter the radius of circle\n");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("area is %.2f",a);
+    return 0;
+}
---
+++ length of string.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=printf("string");
+    printf("length of \"string\" is %d",a);
+    //printf("\n");
+    return 0;
+}
